2961fe0a0da51c8099e20a83f67cf86eba272d23
PetroYatsiv <yatsiv01@gmail.com>
2018-12-10 17:30:21 +0200

    Add Application context.

---
+++ WebApplication1/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=WS-LV-CP2922\\SQLEXPRESS;Initial Catalog=forumDatabase; Integrated Security=True");
+        }
+    }
+}
