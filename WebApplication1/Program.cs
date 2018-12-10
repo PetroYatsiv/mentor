ef5e73891707110b6d5e4c774de5dc26ceb6a3a6
PetroYatsiv <yatsiv01@gmail.com>
2018-12-10 09:56:24 +0200

    Test commit.

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -16,6 +16,7 @@ namespace WebApplication1
         {
             //changes
             //changes after fetch from github
+            //test commit 
             CreateWebHostBuilder(args).Build().Run();
         }
 
