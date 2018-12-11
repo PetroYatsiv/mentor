6351d17c84687a791158190c6952b855783aa269
PetroYatsiv <yatsiv01@gmail.com>
2018-12-11 11:21:38 +0200

    Add DAL project.

--- DataForum/ApplicationContext.cs
+++ DataForum/ApplicationContext.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+using System.Text;
 
-namespace WebApplication1
+namespace DataForum
 {
-    public class ApplicationContext : DbContext
+    class ApplicationContext : DbContext
     {
         public ApplicationContext()
         {
