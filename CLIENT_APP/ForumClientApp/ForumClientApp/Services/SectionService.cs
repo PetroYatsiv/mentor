9f7bafbd8c5c344cc1d06c4dafcbaf3c0133a7f9
petro.yatsiv <petro.yatsiv@eleks.com>
2019-04-03 10:44:34 +0300

    minor changes

--- CLIENT_APP/ForumClientApp/ForumClientApp/Services/SectionService.cs
+++ CLIENT_APP/ForumClientApp/ForumClientApp/Services/SectionService.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using ForumClientApp.Models;
 using Newtonsoft.Json;
-using Forum.Data.Models;
 using ForumClientApp.Contracts;
 
 namespace ForumClientApp.Services
